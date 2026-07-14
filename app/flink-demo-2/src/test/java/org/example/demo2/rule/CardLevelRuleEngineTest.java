74ec50e89ebda6569bbd4d79afd2720800ed246e
mc.chen <mc.chen@iisigroup.com>
2026-07-14 22:36:02 +0800

    update

--- app/flink-demo-2/src/test/java/org/example/demo2/rule/CardLevelRuleEngineTest.java
+++ app/flink-demo-2/src/test/java/org/example/demo2/rule/CardLevelRuleEngineTest.java
@@ -1,7 +1,6 @@
-package org.example.monthly.rule;
+package org.example.demo2.rule;
 
 import org.example.demo2.model.MonthlyConsumptionFact;
-import org.example.demo2.rule.CardLevelRuleEngine;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
